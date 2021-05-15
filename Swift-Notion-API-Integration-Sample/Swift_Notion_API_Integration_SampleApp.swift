6c7bdb6802ba33b8a6898ba3e8702752106f6244
Gerson Janhuel <gersonjanhuel@gmail.com>
2021-05-15 10:26:20 +0700

    Initial Commit

---
+++ Swift-Notion-API-Integration-Sample/Swift_Notion_API_Integration_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Notion_API_Integration_SampleApp.swift
+//  Swift-Notion-API-Integration-Sample
+//
+//  Created by Gerson Janhuel on 15/05/21.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Notion_API_Integration_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
